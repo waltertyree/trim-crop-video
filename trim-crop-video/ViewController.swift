cab4e5368027e413407a18360d7c0b7421c1207c
Walter Tyree <walter@tyreeapps.com>
2021-08-24 17:06:52 -0400

    Initial Commit

---
+++ trim-crop-video/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trim-crop-video
+//
+//  Created by Walter Tyree on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
